08f417e480c7c24114ddda323b64659954f2f5dc
syedmehedi34 <syedmehedi34@gmail.com>
2025-08-03 10:39:22 +0600

    send the application info in the backend

--- src/hooks/useAdmissionModal.jsx
+++ src/hooks/useAdmissionModal.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useAdmissionModal = () => {
+  const axiosPublic = useAxiosPublic();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedUniversity, setSelectedUniversity] = useState(null);
   const [formData, setFormData] = useState({
@@ -11,6 +13,7 @@ const useAdmissionModal = () => {
     address: "",
     dateOfBirth: "",
     image: "",
+    applicationSubmitted: "",
   });
 
   // Prevent background scrolling when modal is open
@@ -36,26 +39,50 @@ const useAdmissionModal = () => {
   };
 
   // Handle form submission
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log({
-      university: {
-        name: selectedUniversity.name,
-        location: selectedUniversity.location,
-        admissionDates: selectedUniversity.admissionDates,
-      },
-      formData,
-    });
-    setIsModalOpen(false);
-    setFormData({
-      candidateName: "",
-      subject: "",
-      candidateEmail: "",
-      candidatePhone: "",
-      address: "",
-      dateOfBirth: "",
-      image: "",
-    });
+    const submissionDate = new Date().toISOString().split("T")[0]; // Get current date in YYYY-MM-DD format
+    const updatedFormData = {
+      ...formData,
+      applicationSubmitted: submissionDate,
+    };
+
+    const university = {
+      universityId: selectedUniversity._id,
+      universityName: selectedUniversity.name,
+      universityLocation: selectedUniversity.location,
+    };
+
+    const applicationData = {
+      ...university,
+      ...updatedFormData,
+    };
+    // console.log(applicationData);
+    //? send data to backend here
+
+    // const sendData = await axiosPublic.post(
+    //   "/apply-admission",
+    //   applicationData
+    // );
+    // alert("Application submitted successfully!");
+    // console.log(sendData);
+    try {
+      const response = await axiosPublic.post(
+        "/apply-admission",
+        applicationData
+      );
+      if (response.data) {
+        console.log(response.data);
+        alert("Application submitted successfully!");
+      }
+    } catch (error) {
+      console.error("Error submitting application:", error);
+      alert("Failed to submit application. Please try again.");
+    }
+
+    //?
+    closeModal();
+    //?
   };
 
   // Open modal with selected university
@@ -80,6 +107,7 @@ const useAdmissionModal = () => {
       address: "",
       dateOfBirth: "",
       image: "",
+      applicationSubmitted: "",
     });
   };
 
